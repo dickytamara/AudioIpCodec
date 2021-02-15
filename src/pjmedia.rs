b5c01346cc0167a2791340ee7abbf90b3e8bf8d5
dickytamara <dickytamara@hotmail.com>
2021-02-15 12:08:07 +0700

    fix unsafe block

--- src/pjmedia.rs
+++ src/pjmedia.rs
@@ -1,41 +1,37 @@
-
-
-use super::pjsua_sys::*;
 use super::pjdefault::AutoCreate;
-use std::os::raw::c_void;
-use std::mem;
+use super::pjsua_sys::*;
+use std::ptr;
 
-impl AutoCreate<pjmedia_srtp_crypto>  for pjmedia_srtp_crypto {
+impl AutoCreate<pjmedia_srtp_crypto> for pjmedia_srtp_crypto {
     fn new() -> pjmedia_srtp_crypto {
         pjmedia_srtp_crypto {
             key: pj_str_t::new(),
             name: pj_str_t::new(),
-            flags: 0
+            flags: 0,
         }
     }
 }
 
-
 impl AutoCreate<pjmedia_vid_stream_rc_config> for pjmedia_vid_stream_rc_config {
-    fn new () -> pjmedia_vid_stream_rc_config {
+    fn new() -> pjmedia_vid_stream_rc_config {
         pjmedia_vid_stream_rc_config {
             method: 0,
-            bandwidth: 0
+            bandwidth: 0,
         }
     }
 }
 
 impl AutoCreate<pjmedia_vid_stream_sk_config> for pjmedia_vid_stream_sk_config {
-    fn new () -> pjmedia_vid_stream_sk_config {
+    fn new() -> pjmedia_vid_stream_sk_config {
         pjmedia_vid_stream_sk_config {
             count: 0,
-            interval: 0
+            interval: 0,
         }
     }
 }
 
 impl AutoCreate<pjmedia_rtcp_fb_cap> for pjmedia_rtcp_fb_cap {
-    fn new () -> pjmedia_rtcp_fb_cap {
+    fn new() -> pjmedia_rtcp_fb_cap {
         pjmedia_rtcp_fb_cap {
             codec_id: pj_str_t::new(),
             type_: 0,
@@ -46,11 +42,11 @@ impl AutoCreate<pjmedia_rtcp_fb_cap> for pjmedia_rtcp_fb_cap {
 }
 
 impl AutoCreate<pjmedia_rtcp_fb_setting> for pjmedia_rtcp_fb_setting {
-    fn new () -> pjmedia_rtcp_fb_setting {
+    fn new() -> pjmedia_rtcp_fb_setting {
         pjmedia_rtcp_fb_setting {
             dont_use_avpf: pj_constants__PJ_FALSE as pj_bool_t,
             cap_count: 0,
-            caps: [ pjmedia_rtcp_fb_cap::new(); 16]
+            caps: [pjmedia_rtcp_fb_cap::new(); 16],
         }
     }
 }
@@ -63,26 +59,20 @@ impl AutoCreate<pjmedia_audio_format_detail> for pjmedia_audio_format_detail {
             frame_time_usec: 0,
             bits_per_sample: 0,
             avg_bps: 0,
-            max_bps: 0
+            max_bps: 0,
         }
     }
 }
 
 impl AutoCreate<pjmedia_rect_size> for pjmedia_rect_size {
     fn new() -> pjmedia_rect_size {
-        pjmedia_rect_size {
-            w: 0,
-            h: 0,
-        }
+        pjmedia_rect_size { w: 0, h: 0 }
     }
 }
 
 impl AutoCreate<pjmedia_ratio> for pjmedia_ratio {
     fn new() -> pjmedia_ratio {
-        pjmedia_ratio {
-            num: 0,
-            denum: 0,
-        }
+        pjmedia_ratio { num: 0, denum: 0 }
     }
 }
 
@@ -98,13 +88,12 @@ impl AutoCreate<pjmedia_video_format_detail> for pjmedia_video_format_detail {
 }
 
 impl AutoCreate<pjmedia_format__bindgen_ty_1> for pjmedia_format__bindgen_ty_1 {
-    fn new () -> pjmedia_format__bindgen_ty_1 {
+    fn new() -> pjmedia_format__bindgen_ty_1 {
         pjmedia_format__bindgen_ty_1 {
             aud: pjmedia_audio_format_detail::new(),
         }
     }
 }
-
 
 impl AutoCreate<pjmedia_format> for pjmedia_format {
     fn new() -> pjmedia_format {
@@ -118,44 +107,40 @@ impl AutoCreate<pjmedia_format> for pjmedia_format {
 }
 
 impl AutoCreate<pjmedia_port_info> for pjmedia_port_info {
-    fn new () -> pjmedia_port_info {
+    fn new() -> pjmedia_port_info {
         pjmedia_port_info {
             name: pj_str_t::new(),
             signature: 0,
             dir: 0,
-            fmt: pjmedia_format::new()
+            fmt: pjmedia_format::new(),
         }
     }
 }
 
 impl AutoCreate<pjmedia_port_port_data> for pjmedia_port_port_data {
     fn new() -> pjmedia_port_port_data {
-        unsafe {
-            let mut void: c_void = mem::zeroed();
-            pjmedia_port_port_data {
-                pdata: &mut void as *mut _,
-                ldata: 0,
-            }
+        pjmedia_port_port_data {
+            pdata: ptr::null_mut(),
+            ldata: 0,
         }
     }
 }
 
 impl AutoCreate<pjmedia_port> for pjmedia_port {
-    fn new () -> pjmedia_port {
+    fn new() -> pjmedia_port {
         pjmedia_port {
             info: pjmedia_port_info::new(),
             port_data: pjmedia_port_port_data::new(),
             get_clock_src: None,
             put_frame: None,
             get_frame: None,
-            on_destroy: None
+            on_destroy: None,
         }
     }
 }
 
-
 impl AutoCreate<pjmedia_transport_op> for pjmedia_transport_op {
-    fn new () -> pjmedia_transport_op {
+    fn new() -> pjmedia_transport_op {
         pjmedia_transport_op {
             get_info: None,
             attach: None,
@@ -169,22 +154,18 @@ impl AutoCreate<pjmedia_transport_op> for pjmedia_transport_op {
             media_stop: None,
             simulate_lost: None,
             destroy: None,
-            attach2: None
+            attach2: None,
         }
     }
 }
 
-
 impl AutoCreate<pjmedia_transport> for pjmedia_transport {
-    fn new () -> pjmedia_transport {
-        unsafe {
-            let mut void: c_void = mem::zeroed();
-            pjmedia_transport {
-                name: [0; 32],
-                type_: 0,
-                op: &mut pjmedia_transport_op::new() as *mut _,
-                user_data: &mut void as *mut _
-            }
+    fn new() -> pjmedia_transport {
+        pjmedia_transport {
+            name: [0; 32],
+            type_: 0,
+            op: &mut pjmedia_transport_op::new() as *mut _,
+            user_data: ptr::null_mut(),
         }
     }
 }
