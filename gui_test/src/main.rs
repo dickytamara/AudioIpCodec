33dcf89b14db82994b313a50c7516c69e3f8c395
dickytamara <dickytamara@hotmail.com>
2021-02-21 15:44:53 +0700

    add env args

--- gui_test/src/main.rs
+++ gui_test/src/main.rs
@@ -1,3 +1,70 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+extern crate gtk;
+extern crate gio;
+
+use gtk::{Builder, LevelBar, prelude::*};
+use gio::prelude::*;
+
+use std::env::args;
+use std::include_str;
+use gtk::{Application, ApplicationWindow, Button};
+
+
 fn main() {
-    println!("Hello, world!");
+    gtk::init().expect("GTK fail to initialize");
+
+    let application = Application::new(
+        Some("com.tamara.open_ip_audio_codec"),
+        gio::ApplicationFlags::FLAGS_NONE
+    ).expect("GTK app fail to initialize.");
+
+    // builder
+    let main_src = include_str!("../../glade/main_ui.glade");
+    let builder: Builder = Builder::from_string(main_src);
+
+    // main window
+    let main_window: gtk::Window = builder.get_object("main_ui").unwrap();
+
+    // cpu level
+    let lvl_cpu: gtk::LevelBar = builder.get_object("lvl_cpu").unwrap();
+
+    // input
+    let lbl_topbar_input: gtk::Label = builder.get_object("lbl_topbar_input").unwrap();
+    let lbl_input_level_l: gtk::Label = builder.get_object("lbl_input_level_l").unwrap();
+    let lbl_input_level_r: gtk::Label = builder.get_object("lbl_input_level_r").unwrap();
+    let lvl_input_l: gtk::LevelBar = builder.get_object("lvl_input_l").unwrap();
+    let lvl_input_r: gtk::LevelBar = builder.get_object("lvl_input_r").unwrap();
+    let btn_input_level_dec: gtk::Button = builder.get_object("btn_input_level_dec").unwrap();
+    let btn_input_level_inc: gtk::Button = builder.get_object("btn_input_level_inc").unwrap();
+    let sldr_input_level: gtk::Scale = builder.get_object("sldr_input_level").unwrap();
+    let lbl_input_device: gtk::Label = builder.get_object("lbl_input_device").unwrap();
+    let cmb_input_device: gtk::ComboBox = builder.get_object("cmb_input_device").unwrap();
+    let btn_input_mute: gtk::Button = builder.get_object("btn_input_mute").unwrap();
+
+    // output
+    let lbl_topbar_input: gtk::Label = builder.get_object("lbl_topbar_input").unwrap();
+    let lbl_output_level_l: gtk::Label = builder.get_object("lbl_output_level_l").unwrap();
+    let lbl_output_level_r: gtk::Label = builder.get_object("lbl_output_level_r").unwrap();
+    let lvl_output_l: gtk::LevelBar = builder.get_object("lvl_output_l").unwrap();
+    let lvl_output_r: gtk::LevelBar = builder.get_object("lvl_output_r").unwrap();
+    let btn_output_level_dec: gtk::Button = builder.get_object("btn_output_level_dec").unwrap();
+    let btn_output_level_inc: gtk::Button = builder.get_object("btn_output_level_inc").unwrap();
+    let sldr_output_level: gtk::Scale = builder.get_object("sldr_output_level").unwrap();
+    let lbl_output_device: gtk::Label = builder.get_object("lbl_output_device").unwrap();
+    let cmb_output_device: gtk::ComboBox = builder.get_object("cmb_output_device").unwrap();
+    let btn_output_mute: gtk::Button = builder.get_object("btn_output_mute").unwrap();
+
+    // init application
+    application.connect_activate(move |app| {
+        // input
+        main_window.set_application(Some(app));
+        main_window.show_all();
+    });
+
+
+    // add env later this only for
+    // sub testing gui
+    application.run(&args().collect::<Vec<_>>());
 }
