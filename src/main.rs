83a22b3df5f8d5640a6af886573d6a74e2d8c69f
dickytamara <dickytamara@hotmail.com>
2021-02-23 14:33:36 +0700

    refactor maintab widget

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ use glib::clone;
 // gui module
 mod dialpad;
 mod audio_line;
+mod maintab;
 
 // sipua module
 mod pjdefault;
@@ -37,63 +38,10 @@ mod sipua;
 
 use dialpad::DialpadWidget;
 use audio_line::AudioLineWidget;
+use maintab::MaintabWidget;
+
 use sipua::*;
 
-
-pub struct MaintabWidget {
-   btnbox: gtk::ButtonBox,
-   stack: gtk::Stack, btn_sip: gtk::Button,
-   btn_account: gtk::Button,
-   btn_settings: gtk::Button,
-   btn_codec: gtk::Button,
-   btn_about: gtk::Button
-}
-
-impl MaintabWidget {
-
-    pub fn new(gtk_builder: &gtk::Builder) -> MaintabWidget {
-
-        MaintabWidget {
-            btnbox: gtk_builder.get_object("btnbox_main").unwrap(),
-            stack: gtk_builder.get_object("stack_main").unwrap(),
-            btn_sip: gtk_builder.get_object("btn_main_sip").unwrap(),
-            btn_account: gtk_builder.get_object("btn_main_account").unwrap(),
-            btn_settings: gtk_builder.get_object("btn_main_settings").unwrap(),
-            btn_codec: gtk_builder.get_object("btn_main_codec").unwrap(),
-            btn_about: gtk_builder.get_object("btn_main_about").unwrap()
-        }
-    }
-
-
-    pub fn init(&mut self) {
-
-        self.btn_sip.connect_clicked(
-          clone!( @weak self.stack as stk => move |_| {
-              stk.set_visible_child_name("page0");
-        }));
-
-        self.btn_account.connect_clicked(
-          clone!( @weak self.stack as stk => move |_| {
-              stk.set_visible_child_name("page1");
-        }));
-
-        self.btn_settings.connect_clicked(
-          clone!( @weak self.stack as stk => move |_| {
-              stk.set_visible_child_name("page2");
-        }));
-
-        self.btn_codec.connect_clicked(
-          clone!( @weak self.stack as stk => move |_| {
-              stk.set_visible_child_name("page3");
-        }));
-
-        self.btn_about.connect_clicked(
-          clone!( @weak self.stack as stk => move |_| {
-              stk.set_visible_child_name("page4");
-        }));
-    }
-
-}
 
 pub struct StatusbarWidget {
     statusbar: gtk::Statusbar,
