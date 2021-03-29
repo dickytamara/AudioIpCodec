d5c626b649d8c913116e231349afc57ba2964166
dickytamara <dickytamara@hotmail.com>
2021-03-29 19:02:16 +0700

    remove unsafe code

--- src/sipua/sip_tones.rs
+++ src/sipua/sip_tones.rs
@@ -151,81 +151,80 @@ impl Drop for SIPRingback {
 
 // this tone gen will alert on incoming call
 pub struct SIPRingtone {
-    tones: [pjmedia_tone_desc; 3],
+    tones: [pjmedia_tone_desc; 32usize],
     slot: i32,
-    port: *mut pjmedia_port,
+    port: Box<*mut pjmedia_port>,
 }
 
 impl SIPRingtone {
     pub fn new() -> SIPRingtone {
         SIPRingtone {
-            tones: [pjmedia_tone_desc::new(); 3],
+            tones: [pjmedia_tone_desc::new(); 32usize],
             slot: -1,
-            port: ptr::null_mut(),
+            port: Box::new(ptr::null_mut()),
         }
     }
 
     pub fn init(&mut self, pool: *mut pj_pool_t, media_config: pjsua_media_config) {
-        unsafe {
-            assert_ne!(pool.is_null(), true);
 
-            let samples_per_frame = media_config.audio_frame_ptime
-                * media_config.clock_rate
-                * media_config.channel_count
-                / 1000;
+        assert_ne!(pool.is_null(), true);
 
-            for tone in self.tones.iter_mut() {
-                tone.freq1 = 800;
-                tone.freq2 = 640;
-                tone.on_msec = 200;
-                tone.off_msec = 100;
-            }
+        let samples_per_frame = media_config.audio_frame_ptime
+            * media_config.clock_rate
+            * media_config.channel_count
+            / 1000;
 
-            self.tones[2].off_msec = 3000;
-
-            let mut name = pj_str(CString::new("ringtone").expect("error").into_raw());
-            let mut status = pjmedia_tonegen_create2(
-                pool,
-                &mut name as *const _,
-                media_config.clock_rate,
-                media_config.channel_count,
-                samples_per_frame,
-                16,
-                PJMEDIA_TONEGEN_LOOP,
-                &mut self.port as *mut _,
-            );
-
-            if status != PJ_SUCCESS as i32 {
-                panic!("cant init SIPRingtone");
-            }
-
-            status = pjsua_conf_add_port(pool, self.port, &mut self.slot as *mut _);
-            if status != PJ_SUCCESS as i32 {
-                panic!("cant init SIPRingtone");
-            }
-
-            status = pjmedia_tonegen_play(
-                self.port,
-                3,
-                self.tones.as_ptr() as *mut pjmedia_tone_desc,
-                PJMEDIA_TONEGEN_LOOP,
-            );
-
-            if status != PJ_SUCCESS as i32 {
-                panic!("cant init SIPRingtone");
-            }
-
-            assert_ne!(self.port.is_null(), true);
-            assert_ne!(self.slot, -1);
-            println!("SIPRingtone init with slot {}", self.slot);
+        for tone in self.tones.iter_mut() {
+            tone.freq1 = 800;
+            tone.freq2 = 640;
+            tone.on_msec = 200;
+            tone.off_msec = 100;
         }
+
+        self.tones[2].off_msec = 3000;
+
+        let mut status = pjmedia::tonegen_create2(
+            pool,
+            String::from("ringtone"),
+            media_config.clock_rate,
+            media_config.channel_count,
+            samples_per_frame,
+            16,
+            PJMEDIA_TONEGEN_LOOP,
+            &mut self.port
+        );
+
+        if status != PJ_SUCCESS as i32 {
+            panic!("cant init SIPRingtone");
+        }
+
+        status = pjsua::conf_add_port(
+            &mut self.port,
+            Some(&mut self.slot));
+
+        if status != PJ_SUCCESS as i32 {
+            panic!("cant init SIPRingtone");
+        }
+
+        status = pjmedia::tonegen_play(
+            &mut self.port,
+            3,
+            &mut self.tones,
+            PJMEDIA_TONEGEN_LOOP
+        );
+
+        if status != PJ_SUCCESS as i32 {
+            panic!("cant init SIPRingtone");
+        }
+
+        assert_ne!(self.port.is_null(), true);
+        assert_ne!(self.slot, -1);
+        println!("SIPRingtone init with slot {}", self.slot);
     }
 }
 
 impl Drop for SIPRingtone {
     fn drop(&mut self) {
-        unsafe {
-            pjmedia_tonegen_stop(self.port);
-        }
+        pjmedia::tonegen_stop(&mut self.port);
     }
 }
