8edadaea8c696fe8f3101d2572567db9b2251c53
dickytamara <dickytamara@hotmail.com>
2021-04-13 08:34:40 +0700

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
@@ -14,54 +13,59 @@ extern crate glib;
 extern crate gdk;
 
 extern crate systemstat;
-
-use gtk::{HeaderBar, TreeView, prelude::*};
-use gio::prelude::*;
-
-use std::{borrow::BorrowMut, env};
-use std::fmt::format;
-use std::include_str;
-use std::thread;
-use std::time::Duration;
-use std::ptr;
-
-use gtk::{Application, ApplicationWindow, Statusbar, Button, Builder, Scale};
-use gtk::ComboBoxText;
-use glib::clone;
-
+extern crate configparser;
 
 // gui module
-mod dialpad;
+mod helper;
+mod about;
 mod audio_line;
+mod codec;
+mod dialpad;
 mod maintab;
 mod header;
 mod status;
 mod account;
 mod settings;
+mod settings_buffer;
+mod settings_call;
+mod settings_ice;
+mod settings_media;
+mod settings_turn;
 
 // sipua module
-mod pjproject;
-mod sipua;
+pub mod pjproject;
+pub mod sipua;
 
-use dialpad::DialpadWidget;
+
+use gtk::prelude::*;
+use gio::prelude::*;
+
+use std::env;
+use std::include_str;
+use std::thread;
+use std::time::Duration;
+use std::ptr;
+
+use gtk::{Application, Builder};
+
+use dialpad::{DialpadWidget, CallButtonState};
 use audio_line::AudioLineWidget;
 use maintab::MaintabWidget;
 use header::HeaderWidget;
 use status::StatusbarWidget;
 use account::AccountWidget;
-use settings::SettingsWidget;
+use settings::{SettingsCurrentActivePage, SettingsWidget};
+use helper::{HelperFileSettings, application_config_path};
+
 
 use sipua::sip_account::SIPAccount;
 use sipua::SIPInviteState;
-use pjproject::pjdefault::AutoCreate;
+use pjproject::utils::AutoCreate;
 
 use pj_sys::*;
 use sipua::*;
 
-use std::ffi::{CString, CStr};
-use std::rc::Rc;
-use std::cell::RefCell;
-use pjproject::pjdefault::ToString;
+use sip_account::SIPAccountExt;
 
 enum SignalLevel { Level( (u32, u32, u32, u32)) }
 
@@ -129,16 +133,15 @@ fn callback_audio_line_widget(sipua: &mut SIPUserAgent, rx_widget: &AudioLineWid
         sip.set_output_level(v);
     });
 
-    tx_widget.on_button_mute_clicked(|| {
-        // todo callback tx mute
-
+    let sip = sipua.clone();
+    rx_widget.on_button_mute_clicked(move |state| {
+        sip.input_mute(state);
     });
 
-    rx_widget.on_button_mute_clicked(|| {
-        // todo callback rx mute
-
+    let sip = sipua.clone();
+    tx_widget.on_button_mute_clicked(move |state| {
+        sip.output_mute(state);
     });
-
 }
 
 // callback dialpad widget
@@ -146,9 +149,17 @@ fn callback_dialpad_widget(sipua: &mut SIPUserAgent, dialpad: &DialpadWidget) {
 
     // button call clicked
     let sip = sipua.clone();
-    dialpad.on_button_call_clicked(move |sip_address| {
+    dialpad.on_button_call_clicked(move | sip_address, state | {
         println!("sip_call_addres : {}", sip_address);
-        sip.call(sip_address);
+
+        match state {
+            CallButtonState::Call => sip.call(sip_address),
+            CallButtonState::Hangup => sip.call_hangup(),
+            CallButtonState::Abort => sip.call_hangup(),
+            CallButtonState::Answer => sip.call_answer(),
+            _ => ()
+        }
+
     });
 
     // callback inv state
@@ -176,8 +187,12 @@ fn callback_dialpad_widget(sipua: &mut SIPUserAgent, dialpad: &DialpadWidget) {
 // callback account widget
 fn callback_account_widget(sipua: &mut SIPUserAgent, account: &AccountWidget) {
 
-    let wid = account.clone();
-    //let ua = sipua.clone();
+    let account_clone = account.clone();
+    account.save_connect_clicked(move ||{
+        account_clone.save(application_config_path());
+    });
+
+    let account_clone = account.clone();
     account.on_btn_connect_clicked(move || {
 
         let mut sipacc = SIPAccount::new();
@@ -185,18 +200,63 @@ fn callback_account_widget(sipua: &mut SIPUserAgent, account: &AccountWidget) {
         sipacc.config_default();
         let mut transport = pjsua_sys::pjsua_transport_config::new();
         transport.port = 4000;
-        sipacc.set_rtp_config(transport);
+        sipacc.set_rtp_cfg(transport);
 
-        sipacc.set_id(wid.get_sip_url());
-        sipacc.set_reg_uri(wid.get_registrar_url());
-        sipacc.set_realm(wid.get_realm());
-        sipacc.set_username(wid.get_username());
-        sipacc.set_password(wid.get_password());
+        sipacc.set_id(account_clone.get_sip_url());
+        sipacc.set_reg_uri(account_clone.get_registrar_url());
+        sipacc.set_realm(account_clone.get_realm());
+        sipacc.set_username(account_clone.get_username());
+        sipacc.set_password(account_clone.get_password());
 
         sipacc.add(true);
     });
-
 }
+
+fn callback_settings_widget(sipua: &mut SIPUserAgent, settings: &SettingsWidget) {
+
+    // save setting for each tab
+    let settings_clone = settings.clone();
+    settings.save_connect_clicked(move |page| {
+        let config_path = application_config_path();
+        match page.unwrap() {
+            SettingsCurrentActivePage::Call => settings_clone.call.save(config_path),
+            SettingsCurrentActivePage::Turn => settings_clone.turn.save(config_path),
+            SettingsCurrentActivePage::Ice => settings_clone.ice.save(config_path),
+            SettingsCurrentActivePage::Buffer => todo!(),
+            SettingsCurrentActivePage::Media => todo!(),
+        };
+    });
+
+    // reset configuration for each tab
+    let settings_clone = settings.clone();
+    settings.reset_connect_clicked(move |page| {
+        match page.unwrap() {
+            SettingsCurrentActivePage::Call => settings_clone.call.reset(),
+            SettingsCurrentActivePage::Turn => settings_clone.turn.reset(),
+            SettingsCurrentActivePage::Ice => settings_clone.ice.reset(),
+            SettingsCurrentActivePage::Buffer => todo!(),
+            SettingsCurrentActivePage::Media => todo!(),
+        };
+    });
+
+    // apply configuration for each tab
+    let settings_clone = settings.clone();
+    let ua = sipua.clone();
+    settings.apply_connect_clicked(move |page| {
+        match page.unwrap() {
+            SettingsCurrentActivePage::Call => {
+
+            },
+            SettingsCurrentActivePage::Turn => {
+                ua.set_autoanswer(settings_clone.call.get_autoanswer());
+            },
+            SettingsCurrentActivePage::Ice => { todo!(); },
+            SettingsCurrentActivePage::Buffer => { todo!(); },
+            SettingsCurrentActivePage::Media => { todo!(); },
+        }
+    });
+}
+
 
 fn main() {
     gtk::init()
@@ -212,36 +272,24 @@ fn main() {
 
     // builder
     let main_src = include_str!("../glade/main_ui.glade");
-    let builder: Builder = Builder::from_string(main_src);
+    let builder= Builder::from_string(main_src);
 
     let main_window: gtk::ApplicationWindow = builder.get_object("main_ui").unwrap();
 
     let rx_widget = audio_line::create_transmit_widget(&builder);
     let tx_widget = audio_line::create_receive_widget(&builder);
-
-    let maintab_widget: MaintabWidget = MaintabWidget::new(&builder);
-
-    let statusbar_widget: StatusbarWidget = StatusbarWidget::new(&builder);
-
-    let headerbar_widget: HeaderWidget = HeaderWidget::new(&builder);
-
+    let maintab_widget= MaintabWidget::new(&builder);
+    let statusbar_widget = StatusbarWidget::new(&builder);
+    let headerbar_widget = HeaderWidget::new(&builder);
     let dialpad_widget = DialpadWidget::new(&builder);
+    let account_widget = AccountWidget::new(&builder);
+    let settings_widget= SettingsWidget::new(&builder);
 
-    let account_widget: AccountWidget = AccountWidget::new(&builder);
-    let settings_widget: SettingsWidget = SettingsWidget::new(&builder);
-
-    // initialize
-    rx_widget.init();
-    tx_widget.init();
-    maintab_widget.init();
-    headerbar_widget.init();
-    dialpad_widget.init();
-    account_widget.init();
-    settings_widget.init();
-
+    // set callback
     callback_audio_line_widget(&mut sipua, &rx_widget, &tx_widget);
     callback_dialpad_widget(&mut sipua, &dialpad_widget);
     callback_account_widget(&mut sipua, &account_widget);
+    callback_settings_widget(&mut sipua, &settings_widget);
 
     // test call data
     dialpad_widget.set_call_address_text(String::from("sip://@27.50.19.174"));
