b8831c27c2a5cd834b2332272b8656bb3c094680
dickytamara <dickytamara@hotmail.com>
2021-04-16 13:08:31 +0700

    update turn code

--- src/settings_turn.rs
+++ src/settings_turn.rs
@@ -1,6 +1,6 @@
 
 use gtk::prelude::*;
-use gtk::{Label, SpinButton, ComboBoxText, Entry, Switch, Builder};
+use gtk::{Label, ComboBoxText, Entry, Switch, Builder};
 use std::cell::RefCell;
 use std::path::PathBuf;
 
@@ -11,42 +11,30 @@ use configparser::ini::Ini;
 #[derive(Clone)]
 pub struct SettingsTurnWidgetStorage {
     lbl_use_turn: Label,
-    lbl_tcp: Label,
-    lbl_rtcp_multiplexing: Label,
+    lbl_transport: Label,
     lbl_server: Label,
-    lbl_port: Label,
     lbl_username: Label,
     lbl_password: Label,
-    lbl_keyring: Label,
     swt_use_turn: Switch,
-    swt_turn_tcp: Switch,
-    swt_turn_rtcp_multiplexing: Switch,
-    spn_turn_port: SpinButton,
-    ent_turn_server: Entry,
-    ent_turn_username: Entry,
-    ent_turn_password: Entry,
-    cmb_turn_keyring: ComboBoxText,
+    cmb_transport: ComboBoxText,
+    ent_server: Entry,
+    ent_username: Entry,
+    ent_password: Entry,
 }
 
 impl SettingsTurnWidgetStorage {
     pub fn new(gtk_builder: &Builder) -> Self {
         SettingsTurnWidgetStorage {
             lbl_use_turn: gtk_builder.get_object("lbl_use_turn").unwrap(),
-            lbl_tcp: gtk_builder.get_object("lbl_turn_tcp").unwrap(),
-            lbl_rtcp_multiplexing: gtk_builder.get_object("lbl_turn_rtcp_multiplexing").unwrap(),
+            lbl_transport: gtk_builder.get_object("lbl_turn_transport").unwrap(),
             lbl_server: gtk_builder.get_object("lbl_turn_server").unwrap(),
-            lbl_port: gtk_builder.get_object("lbl_turn_port").unwrap(),
             lbl_username: gtk_builder.get_object("lbl_turn_username").unwrap(),
             lbl_password: gtk_builder.get_object("lbl_turn_password").unwrap(),
-            lbl_keyring: gtk_builder.get_object("lbl_turn_keyring").unwrap(),
             swt_use_turn: gtk_builder.get_object("swt_use_turn").unwrap(),
-            swt_turn_tcp: gtk_builder.get_object("swt_turn_tcp").unwrap(),
-            swt_turn_rtcp_multiplexing: gtk_builder.get_object("swt_turn_rtcp_multiplexing").unwrap(),
-            spn_turn_port: gtk_builder.get_object("spn_turn_port").unwrap(),
-            ent_turn_server: gtk_builder.get_object("ent_turn_server").unwrap(),
-            ent_turn_username: gtk_builder.get_object("ent_turn_username").unwrap(),
-            ent_turn_password: gtk_builder.get_object("ent_turn_password").unwrap(),
-            cmb_turn_keyring: gtk_builder.get_object("cmb_turn_keyring").unwrap(),
+            cmb_transport: gtk_builder.get_object("cmb_turn_transport").unwrap(),
+            ent_server: gtk_builder.get_object("ent_turn_server").unwrap(),
+            ent_username: gtk_builder.get_object("ent_turn_username").unwrap(),
+            ent_password: gtk_builder.get_object("ent_turn_password").unwrap(),
         }
     }
 }
@@ -65,10 +53,6 @@ impl SettingsTurnWidget {
         };
 
         // set spin button turn port server
-        result.ctx.borrow().spn_turn_port.set_digits(0);
-        result.ctx.borrow().spn_turn_port.set_range(3478_f64, 65535_f64);
-        result.ctx.borrow().spn_turn_port.set_increments(1_f64, 5_f64);
-
         let this = result.clone();
         result.ctx.borrow().swt_use_turn.connect_property_active_notify(move |s| {
             this.set_use_turn(s.get_state())
@@ -82,99 +66,64 @@ impl SettingsTurnWidget {
         // set default value for turn properties
         let context = self.ctx.borrow();
         context.swt_use_turn.set_state(false);
-        context.swt_turn_tcp.set_state(false);
-        context.swt_turn_rtcp_multiplexing.set_state(false);
 
-        context.spn_turn_port.set_value(3478_f64);
-        context.ent_turn_server.set_text("");
-        context.ent_turn_username.set_text("");
-        context.ent_turn_password.set_text("");
-        context.cmb_turn_keyring.set_active_id(Some("SDES"));
+        context.ent_server.set_text("");
+        context.ent_username.set_text("");
+        context.ent_password.set_text("");
         self.set_use_turn(false);
     }
 
     pub fn set_use_turn(&self, value: bool) {
-        let context = self.ctx.borrow();
-
         // label update
-        context.lbl_tcp.set_sensitive(value);
-        context.lbl_rtcp_multiplexing.set_sensitive(value);
-        context.lbl_server.set_sensitive(value);
-        context.lbl_port.set_sensitive(value);
-        context.lbl_username.set_sensitive(value);
-        context.lbl_password.set_sensitive(value);
-        context.lbl_keyring.set_sensitive(value);
+        self.ctx.borrow().lbl_transport.set_sensitive(value);
+        self.ctx.borrow().lbl_server.set_sensitive(value);
+        self.ctx.borrow().lbl_username.set_sensitive(value);
+        self.ctx.borrow().lbl_password.set_sensitive(value);
 
-        context.swt_turn_tcp.set_sensitive(value);
-        context.swt_turn_rtcp_multiplexing.set_sensitive(value);
-        context.ent_turn_server.set_sensitive(value);
-        context.spn_turn_port.set_sensitive(value);
-        context.ent_turn_username.set_sensitive(value);
-        context.ent_turn_password.set_sensitive(value);
-        context.cmb_turn_keyring.set_sensitive(value);
+        // input update
+        self.ctx.borrow().cmb_transport.set_sensitive(value);
+        self.ctx.borrow().ent_server.set_sensitive(value);
+        self.ctx.borrow().ent_username.set_sensitive(value);
+        self.ctx.borrow().ent_password.set_sensitive(value);
     }
 
     pub fn get_use_turn(&self) -> bool {
         self.ctx.borrow().swt_use_turn.get_state()
     }
 
-    pub fn set_use_tcp(&self, value: bool) {
-        self.ctx.borrow().swt_turn_tcp.set_state(value);
+    pub fn set_transport(&self, value: u32) {
+        self.ctx.borrow_mut().cmb_transport.set_active(Some(value -1));
     }
 
-    pub fn get_use_tcp(&self) -> bool {
-        self.ctx.borrow().swt_turn_tcp.get_state()
-    }
-
-    pub fn set_use_rtcp_multiplexing(&self, value: bool) {
-        self.ctx.borrow().swt_turn_rtcp_multiplexing.set_state(value);
-    }
-
-    pub fn get_use_rtcp_multiplexing(&self) -> bool {
-        self.ctx.borrow().swt_turn_rtcp_multiplexing.get_state()
-    }
-
-    pub fn get_server(&self) -> String {
-        self.ctx.borrow().ent_turn_server.get_text().to_string().clone()
-    }
-
-    pub fn set_server(&self, value: String) {
-        self.ctx.borrow().ent_turn_server.set_text(value.as_str());
-    }
-
-    pub fn get_port(&self) -> f64 {
-        self.ctx.borrow().spn_turn_port.get_value()
-    }
-
-    pub fn set_port(&self, value: f64) {
-        self.ctx.borrow().spn_turn_port.set_value(value);
-    }
-
-    pub fn set_username(&self, value: String) {
-        self.ctx.borrow().ent_turn_username.set_text(value.as_str());
-    }
-
-    pub fn get_username(&self) -> String {
-        self.ctx.borrow().ent_turn_username.get_text().to_string().clone()
-    }
-
-    pub fn set_password(&self, value: String) {
-        self.ctx.borrow().ent_turn_password.set_text(value.as_str());
-    }
-
-    pub fn get_password(&self) -> String {
-        self.ctx.borrow().ent_turn_password.get_text().to_string().clone()
-    }
-
-    pub fn get_srtp_keyring(&self) -> u32 {
-        match self.ctx.borrow().cmb_turn_keyring.get_active() {
+    pub fn get_transport(&self) -> u32 {
+        match self.ctx.borrow().cmb_transport.get_active() {
             Some(value) => value + 1,
-            None => 0,
+            None => 0
         }
     }
 
-    pub fn set_srtp_keyring(&self, value: u32) {
-        self.ctx.borrow().cmb_turn_keyring.set_active(Some(value -1));
+    pub fn get_server(&self) -> String {
+        self.ctx.borrow().ent_server.get_text().to_string().clone()
+    }
+
+    pub fn set_server(&self, value: String) {
+        self.ctx.borrow().ent_server.set_text(value.as_str());
+    }
+
+    pub fn set_username(&self, value: String) {
+        self.ctx.borrow().ent_username.set_text(value.as_str());
+    }
+
+    pub fn get_username(&self) -> String {
+        self.ctx.borrow().ent_username.get_text().to_string().clone()
+    }
+
+    pub fn set_password(&self, value: String) {
+        self.ctx.borrow().ent_password.set_text(value.as_str());
+    }
+
+    pub fn get_password(&self) -> String {
+        self.ctx.borrow().ent_password.get_text().to_string().clone()
     }
 
 }
@@ -186,37 +135,22 @@ impl HelperFileSettings for SettingsTurnWidget {
         let mut config = Ini::new();
         config.load(path.to_str().unwrap()).unwrap();
 
-        self.set_use_turn(config.get("turn", "use_turn").unwrap_or(String::from("false")).parse().unwrap());
-        self.set_use_tcp(config.get("turn", "use_tcp").unwrap_or(String::from("false")).parse().unwrap());
-        self.set_use_rtcp_multiplexing(config.get("stun", "use_rtcp_multiplexing").unwrap().parse().unwrap());
+        self.set_use_turn (config.get("turn", "use_turn").unwrap_or(String::from("false")).parse().unwrap());
+        self.set_transport(config.get("turn", "transport").unwrap_or(String::from("1")).parse().unwrap());
         self.set_server(config.get("turn", "server").unwrap());
-        self.set_port(config.get("turn", "port").unwrap().parse().unwrap());
         self.set_username(config.get("turn", "username").unwrap());
         self.set_password(config.get("turn", "password").unwrap());
-        self.set_srtp_keyring(config.get("turn", "srtp_keyring").unwrap().parse().unwrap());
     }
 
     fn save(&self, path: PathBuf) {
         let mut config = Ini::new();
         config.load(path.to_str().unwrap()).unwrap();
 
-        let use_turn = self.get_use_turn();
-        let use_tcp = self.get_use_tcp();
-        let use_rtcp_multiplexing = self.get_use_rtcp_multiplexing();
-        let server = self.get_server();
-        let port = self.get_port();
-        let username = self.get_username();
-        let password = self.get_password();
-        let srtp_keyring = self.get_srtp_keyring();
-
         config.set("stun", "use_turn", Some(self.get_use_turn().to_string()));
-        config.set("stun", "use_tcp", Some(self.get_use_tcp().to_string()));
-        config.set("stun", "use_rtcp_multiplexing", Some(self.get_use_rtcp_multiplexing().to_string()));
+        config.set("turn", "transport", Some(self.get_transport().to_string()));
         config.set("stun", "server", Some(self.get_server()));
-        config.set("stun", "port", Some(self.get_port().to_string()));
         config.set("stun", "username", Some(self.get_username()));
         config.set("stun", "password", Some(self.get_password()));
-        config.set("stun", "srtp_keyring", Some(self.get_srtp_keyring().to_string()));
 
         config.write(path.to_str().unwrap()).unwrap();
     }
