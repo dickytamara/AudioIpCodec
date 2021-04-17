6a2eb0cd59b1349b04b783af6fe03ea7c160fb95
dickytamara <dickytamara@hotmail.com>
2021-04-17 13:06:06 +0700

    update code

--- src/settings_media.rs
+++ src/settings_media.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Label, Entry, Switch, Builder};
+use gtk::{Label, ComboBoxText, Switch, Builder};
 use std::cell::RefCell;
 use std::path::PathBuf;
 
@@ -9,14 +9,20 @@ use configparser::ini::Ini;
 
 #[derive(Clone)]
 pub struct SettingsMediaStorage {
-
+    lbl_rtcp_multiplexing: Label,
+    lbl_keyring: Label,
+    swt_rtcp_multiplexing: Switch,
+    cmb_keyring: ComboBoxText
 }
 
 
 impl SettingsMediaStorage {
     pub fn new(gtk_builder: &Builder) -> Self {
         SettingsMediaStorage {
-            // TODO
+            lbl_rtcp_multiplexing: gtk_builder.get_object("lbl_media_rtcp_multiplexing").unwrap(),
+            lbl_keyring: gtk_builder.get_object("lbl_keyring").unwrap(),
+            swt_rtcp_multiplexing: gtk_builder.get_object("swt_media_rtcp_multiplexing").unwrap(),
+            cmb_keyring: gtk_builder.get_object("cmb_media_keyring").unwrap()
         }
     }
 }
@@ -37,7 +43,27 @@ impl SettingsMediaWidget {
     }
 
     pub fn reset(&self) {
-        todo!();
+        self.ctx.borrow().swt_rtcp_multiplexing.set_state(false);
+        self.ctx.borrow().cmb_keyring.set_active(Some(0));
+    }
+
+    pub fn set_rtcp_multiplexing(&self, value: bool) {
+        self.ctx.borrow_mut().swt_rtcp_multiplexing.set_state(value);
+    }
+
+    pub fn get_rtcp_multiplexing(&self) -> bool {
+        self.ctx.borrow_mut().swt_rtcp_multiplexing.get_state()
+    }
+
+    pub fn set_keyring(&self, value: u32) {
+        self.ctx.borrow().cmb_keyring.set_active(Some(value -1));
+    }
+
+    pub fn get_keyring(&self) -> u32 {
+        match self.ctx.borrow().cmb_keyring.get_active() {
+            Some(value) => value +1,
+            None => 0
+        }
     }
 }
 
@@ -47,10 +73,17 @@ impl HelperFileSettings for SettingsMediaWidget {
         let mut config = Ini::new();
         config.load(path.to_str().unwrap()).unwrap();
 
-        todo!()
+        self.set_rtcp_multiplexing(config.get("media", "rtcp_multiplexing").unwrap_or(String::from("false")).parse().unwrap());
+        self.set_keyring(config.get("media", "keyring").unwrap_or(String::from("0")).parse().unwrap());
     }
 
     fn save(&self, path: PathBuf) {
-        todo!()
+        let mut config = Ini::new();
+        config.load(path.to_str().unwrap()).unwrap();
+
+        config.set("media", "rtcp_multiplexing", Some(self.get_rtcp_multiplexing().to_string()));
+        config.set("media", "keyring", Some(self.get_keyring().to_string()));
+
+        config.write(path.to_str().unwrap()).unwrap();
     }
 }
